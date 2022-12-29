7fce25a3b0e60a1f8341762749cf3f61f4d8620a
shibdo9 <121630901+shibdo9@users.noreply.github.com>
2022-12-29 18:32:56 +0000

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `What's up, ${myName}`;
+    }
+  }
+  
+  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `What's up, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
